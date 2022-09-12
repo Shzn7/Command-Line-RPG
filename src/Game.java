fd557278ce6eefc5b7189e31f22689c578116a06
Shaazaan Majeed <u7283219@anu.edu.au>
2022-09-12 17:06:21 +1000

    Polish reading game configuration from a JSON file

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,7 @@
-import java.io.File;
-
 public class Game {
-    private static final String JSON_FILE = "src/gameConfiguration.json";
 
     public static void main(String[] args) {
-        File jsonFile = new File(JSON_FILE);
-        LoadGameConfiguration gameConfig = new LoadGameConfiguration().setGameConfigFromJsonFile(jsonFile);
+        LoadGameConfiguration gameConfig = new LoadGameConfiguration().setGameConfigFromJsonFile();
 
         System.out.println("Game Path     : " + gameConfig.getGamePath());
         System.out.println("Encounter Path: " + gameConfig.getEncounterPath());
--- src/LoadGameConfiguration.java
+++ src/LoadGameConfiguration.java
@@ -1,4 +1,10 @@
-// JSON File Dependency Imports
+/**
+ * LoadGameConfiguration.java class used to read game data from JSON file setting String variables
+ * to represent data encodings
+ *
+ * @author Shaazaan Majeed
+ */
+
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
@@ -6,6 +12,15 @@ import java.io.File;
 import java.io.FileReader;
 
 public class LoadGameConfiguration {
+    private static final String JSON_FILE = "src/gameConfiguration.json";
+
+    /**
+     * Variables to store all game configuration data
+     *
+     * correctGamePath: N=North, E=East, S=South, W=West
+     * encounterPath: N=North, E=East, S=South, W=West
+     * encounterType: E=Enemy, N=NPC, R=Reward
+     */
     private String correctGamePath;
     private String encounterPath;
     private String encounterType;
@@ -13,27 +28,48 @@ public class LoadGameConfiguration {
     public LoadGameConfiguration() {
     }
 
-    public LoadGameConfiguration setGameConfigFromJsonFile(File file) {
+    /**
+     * This method sets all game configuration variables using the JSON parser by Google (Gson) JSON File
+     * specified by the JSON_FILE global final variable
+     *
+     * @author Shaazaan Majeed
+     *
+     * @return LoadGameConfiguration, setting all the private variables with their respective encodings as read by the JSON File
+     */
+    public LoadGameConfiguration setGameConfigFromJsonFile() {
+        File file = new File(JSON_FILE);
         Gson gson = new Gson();
         JsonReader jsonReader = null;
-
         try {
             jsonReader = new JsonReader(new FileReader(file));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        LoadGameConfiguration gameConfig = gson.fromJson(jsonReader, LoadGameConfiguration.class);
-        return gameConfig;
+        if (jsonReader == null) return this;
+        return gson.fromJson(jsonReader, LoadGameConfiguration.class);
     }
 
+
+    /**
+     * Getter method to retrieve the correctGamePath
+     * @return correctGamePath String
+     */
     public String getGamePath() {
         return correctGamePath;
     }
 
+    /**
+     * Getter method to retrieve the encounterPath
+     * @return encounterPath String
+     */
     public String getEncounterPath() {
         return encounterPath;
     }
 
+    /**
+     * Getter method to retrieve the encounterType
+     * @return encounterType String
+     */
     public String getEncounterType() {
         return encounterType;
     }
