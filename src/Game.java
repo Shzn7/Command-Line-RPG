9cb76af10d5db112cbed6c3023b1e4b21c43a819
Deni Lanc <u7284728@anu.edu.au>
2022-09-16 19:39:41 +1000

    Created character selection prompts and options

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class Game {
     private static final String JSON_FILE = "src/gameConfiguration.json";
@@ -10,6 +11,32 @@ public class Game {
         System.out.println("Game Path     : " + gameConfig.getGamePath());
         System.out.println("Encounter Path: " + gameConfig.getEncounterPath());
         System.out.println("Encounter Type: " + gameConfig.getEncounterType());
+
+        selectCharacter();
+    }
+
+    /**
+     * Called the first time a user runs the game.
+     * Allows user to choose the character they want to play as.
+     */
+    public static void selectCharacter() {
+
+        System.out.println("\nPick your character:");
+        System.out.println("a) Ninja");
+        System.out.println("b) Wizard");
+        System.out.println("c) Pirate");
+
+        Scanner read = new Scanner(System.in);
+        String input = read.nextLine();
+        switch (input.toUpperCase()) {
+            case "A", "NINJA" -> System.out.println("you picked NINJA");
+            case "B", "WIZARD" -> System.out.println("you picked WIZARD");
+            case "C", "PIRATE" -> System.out.println("you picked PIRATE");
+            default -> {
+                System.out.println("Not a valid input try again");
+                selectCharacter();
+            }
+        }
     }
 
     public void move(){
