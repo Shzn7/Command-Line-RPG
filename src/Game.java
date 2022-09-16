31f746cc5ee1f1a56034f0aae2a1e846eaf91615
“Alex <u7287093@anu.edu.au>
2022-09-16 13:43:28 +1000

    Created a working CommandHandler Class, filled in the move class and created more commandEnum types, including javaDoc comments and tests.

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,20 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class Game {
     private static final String JSON_FILE = "src/gameConfiguration.json";
+
+    /**
+     * Variables to store all game configuration data
+     */
+    private static String gamePath  = "";
+    private static String encounterPath  = "";
+    private static String encounterType  = "";
+
+    /**
+     * Index to store the place along the gameString the user is at.
+     */
+    static int index = 0;
 
     public static void main(String[] args) {
         File gameConfigFile = new File(JSON_FILE);
@@ -10,9 +23,74 @@ public class Game {
         System.out.println("Game Path     : " + gameConfig.getGamePath());
         System.out.println("Encounter Path: " + gameConfig.getEncounterPath());
         System.out.println("Encounter Type: " + gameConfig.getEncounterType());
+
+        gamePath =  gameConfig.getGamePath();
+        encounterPath = gameConfig.getEncounterPath();
+        encounterType = gameConfig.getEncounterType();
+
+        move();
     }
 
-    public void move(){
+    public static void move(){
 
+        while(true) {
+            /*
+            * Will end the game once the user has reached the end of the gameString
+             */
+            if (index >= gamePath.length()) {
+                System.out.println("You made it to the end, congrats!");
+                break;
+            }
+
+            Scanner read = new Scanner(System.in);
+            String input = read.nextLine();
+
+
+            /*
+             * Uses CommandHandler to interpret the command from the input text.
+             */
+            CommandsEnum command = new CommandHandler().processCommand(input);
+
+            if (command.equals(CommandsEnum.QUIT)) {break;}
+
+            //Temporary outlets for SAVE, LOAD, HELP, INV, STATS
+            if (command.equals(CommandsEnum.SAVE) || command.equals(CommandsEnum.LOAD)
+                    || command.equals(CommandsEnum.HELP) || command.equals(CommandsEnum.INV) || command.equals(CommandsEnum.STATS)) {
+                System.out.println(input + " is under construction, please try again.");}
+
+
+            if (command.equals(CommandsEnum.NULL)) {
+                System.out.println(input + " was an invalid input, please try again.");
+            }
+
+            /*
+             * Once it is determined that the user submitted a valid string, the program will find the current CommandEnum
+             * value of the gamePath, encounterPath and encounterType path, to determine whether the user moves forward, encounters
+             * an object or goes the wrong way.
+             */
+
+            CommandsEnum correctPath = (new CommandHandler().processCommand(gamePath.charAt(index) + ""));
+            CommandsEnum eventPath = (new CommandHandler().processCommand(encounterPath.charAt(index) + ""));
+            CommandsEnum eventType = (new CommandHandler().processEncounter(encounterType.charAt(index)));
+
+            if (command.equals(correctPath)) {
+                index++;
+                System.out.println(input + " was the correct path, you continue on your journey.");
+            } else {
+                if (command.equals(eventPath)) {
+
+                    switch (eventPath) {
+                        case ENEMY -> Interactions.battle();
+                        case NPC -> Interactions.talkWithNPC();
+                        case REWARD -> Interactions.reward();
+                    }
+
+                    System.out.println(input + " was the encounter path, you would have a " + eventType);
+                } else {
+                    System.out.println(input + " is the wrong way! Try again.");
+                }
+            }
+
+        }
     }
 }
